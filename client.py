7eb14b1dbe859168641372da15ce0e564d36a824
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-07-18 11:43:56 +0500

    Modifications

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
+from flwr.client import NumPyClient, start_client
 import tensorflow as tf
-import flwr as fl
+
 
 model = tf.keras.applications.MobileNetV2((32,32,3), classes=10, weights=None)
 model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
@@ -8,19 +9,22 @@ model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
 model.fit(x_train, y_train, epochs=1, batch_size=32)
 
-class FlowerClient(fl.client.NumPyClient):
-
+# Define Flower client
+class FlowerClient(NumPyClient):
     def get_parameters(self, config):
         return model.get_weights()
-    
+
     def fit(self, parameters, config):
         model.set_weights(parameters)
         model.fit(x_train, y_train, epochs=1, batch_size=32)
         return model.get_weights(), len(x_train), {}
-    
+
     def evaluate(self, parameters, config):
         model.set_weights(parameters)
         loss, accuracy = model.evaluate(x_test, y_test)
         return loss, len(x_test), {"accuracy": accuracy}
 
-fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=FlowerClient)
+start_client(
+        server_address="127.0.0.1:8080",
+        client=FlowerClient().to_client(),
+    )
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import flwr as fl
+from flwr.server import ServerConfig
+from flwr.server.strategy import FedMedian
 
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
-    strategy=fl.server.strategy.FedMedian(),
+    config=ServerConfig(num_rounds=3),
+    strategy=FedMedian(),
     )
